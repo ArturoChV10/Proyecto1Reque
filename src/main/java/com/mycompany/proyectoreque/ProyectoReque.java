6d04634c4209d76e8fe7a5b6ad47f24f7bae1fed
ArturoChV10 <chavarriavillareviajorgearturo@gmail.com>
2025-03-09 15:36:54 -0600

    Primer commit, se agregó la interfaz del menú principal junto a ciertos detalles en los botones principales

---
+++ src/main/java/com/mycompany/proyectoreque/ProyectoReque.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoreque;
+
+/**
+ *
+ * @author chava
+ */
+public class ProyectoReque {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        homeMenu.main(args);
+    }
+}
