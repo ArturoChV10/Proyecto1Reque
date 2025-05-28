c8bd0d8a30547eb8eb90741c3f185941dd20150a
ArturoChV10 <chavarriavillareviajorgearturo@gmail.com>
2025-05-27 23:05:19 -0600

    Se creó la carpeta 'correccion', en la cual se realizaron varios cambios a newAppointment, no quería eliminar lo pasado, entonces lo dejé aparte

---
+++ correccion/src/main/java/com/mycompany/proyectoreque/ProyectoReque.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoreque;
+
+/**
+ *
+ * @author chava
+ */
+public class ProyectoReque {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //homeMenu.main(args);
+        Email.main(args);
+        
+    }
+}
